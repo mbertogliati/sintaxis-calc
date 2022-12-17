3e19968e15a661a9158805cde0c84515f31674cb
mbertogliati <mbertogliati@frba.utn.edu.ar>
2022-12-17 18:55:55 -0300

    agrego makefile para construir la calculadora

--- TP-5/main.c
+++ TP-5/main.c
@@ -2,6 +2,11 @@
 #include "parser.h"
 #include "calc.h"
 
+
+//Matias Nahuel Cotens Legajo: 202682-0
+//Guadalupe García
+//Mateo Bertogliati Legajo: 203413-0
+
 char *token_names[] = {"EOF", "Entero", "Real", "Identificador", "Operador de asignacion", "Operador aritmetico", "NL", "Parentesis", "SALIR", "CTE", "VAR"};
 int main(void) {
 	agregar_constantes();
